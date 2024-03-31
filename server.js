6d90ccef2879f03e066e9f6de9bcc8fdab854227
dysphie <dysphie@gmail.com>
2024-03-31 14:04:27 -0300

    add rate limiter

--- server.js
+++ server.js
@@ -3,13 +3,23 @@ import dotenv from 'dotenv'
 import { createTables } from './queries.js'
 import routes from './routes/index.js'
 
+import { rateLimit } from 'express-rate-limit'
+
 dotenv.config()
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+})
 
 const app = express()
 const PORT = 3000
 
 await createTables()
 
+app.use(limiter)
 app.use(express.json())
 app.use('/tiers', routes.tiers)
 app.use('/maps', routes.maps)
